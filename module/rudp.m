4347df157693bab1c05a2c3d9eb8bf69c46b17cc
kabbi <userdima@gmail.com>
2013-04-27 15:37:12 +0300

    Oops, forgot to add module description file for rudp

--- module/rudp.m
+++ module/rudp.m
@@ -1,5 +1,7 @@
 Rudp: module
 {
+    PATH:   con "/dis/lib/rudp.dis";
+
     # data, timeout, retry count
     new: fn(connfd: ref Sys->FD, tchan: chan of (array of byte, int, int)): chan of array of byte;
     init: fn();
