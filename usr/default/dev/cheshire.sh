2bc1b22482574375887ced7720c44ebd7f780492
kabbi <userdima@gmail.com>
2013-05-02 00:21:31 +0300

    Updated helper scripts for rudp and cheshire.
    Included rudp in mkfile.

--- usr/default/dev/cheshire.sh
+++ usr/default/dev/cheshire.sh
@@ -4,9 +4,8 @@ fn ml {
 }
 fn m {
 	mount {ch} /wonderland
-	bind /net /wonderland/cheshire/host/net
 }
 fn as {cat /server > cheshire/addserver}
 fn ch {cheshire udp!127.0.0.1!12100 /lib/dht/neis}
-fn k {kill Cheshire Nametree Styxservers Dht}
+fn k {kill Cheshire Nametree Styxservers Dht Rudp Dhtfs Logfile}
 fn u {unmount /wonderland}
--- usr/default/dev/rudp.sh
+++ usr/default/dev/rudp.sh
@@ -1,5 +1,7 @@
 fn cr {limbo -g -o /dis/lib/rudp.dis /appl/lib/rudp.b}
 fn ct {limbo -g -o /dis/rudpping.dis /appl/cmd/rudpping.b}
-fn rping {rudpping udp!127.0.0.1!12200 udp!127.0.0.1!12300 -r}
-fn sping {rudpping udp!127.0.0.1!12300 udp!127.0.0.1!12200 -s $1}
+fn c {cr;ct}
+fn rping {rudpping udp!127.0.0.1!11000 udp!127.0.0.1!11001 -r}
+fn sping {rudpping udp!127.0.0.1!11001 udp!127.0.0.1!11000 -s $1}
+fn wping {rudpping udp!127.0.0.1!11002 udp!192.40.56.191!12000 -s $1}
 fn k {kill Rudp Rudpping}
