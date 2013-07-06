1045f5e10a87caaa8cd090114594de791f4f6408
kabbi <userdima@gmail.com>
2013-07-06 23:20:42 +0300

    Updated cheshire dev helpers

--- usr/default/dev/cheshire.sh
+++ usr/default/dev/cheshire.sh
@@ -1,11 +1,15 @@
+# clear the 'dht neighbours' file
+fn n {echo > /lib/dht/neis}
+# compile cheshire
 fn c {limbo -g -o /dis/cheshire.dis /appl/cmd/cheshire.b}
-fn ml {
-	mount {styxmon {ch}} /wonderland
-}
-fn m {
-	mount {ch} /wonderland
-}
-fn as {cat /server > cheshire/addserver}
-fn ch {cheshire udp!127.0.0.1!12100 /lib/dht/neis}
-fn k {kill Cheshire Nametree Styxservers Dht Rudp Dhtfs Logfile}
-fn u {unmount /wonderland}
+# mount cheshire
+fn m {mount {ch} /wonderland}
+# debug helper functions to add predefined server/program
+fn as {cat /server > /wonderland/cheshire/addserver}
+fn ap {cat /program > /wonderland/cheshire/addprogram}
+# main executable, set verbose var to be more verbose
+fn ch {cheshire udp!127.0.0.1!12100 /lib/dht/neis $verbose}
+# shutdown helpers, should be called in that correct order
+# WARNING: this will kill all the styxservers, clients, dhts and exports!
+fn k {kill Cheshire Nametree Styxservers Dht Rudp Dhtfs Logfile Styx Export}
+fn u {unmount /wonderland/cheshire/dht; unmount /wonderland}
