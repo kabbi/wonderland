77816f571e1257c7742b2707c65fbd3a9b976a8d
kabbi <userdima@gmail.com>
2013-07-10 13:47:20 +0300

    Some helpers and files for vkfs

---
+++ usr/default/dev/vkfs.sh
@@ -0,0 +1,5 @@
+fn c {limbo -g -o /dis/vkfs.dis /appl/cmd/vkfs.b}
+fn m {vkfs /lib/vkauth /mnt/vkfs}
+fn ml {vkfs -D /lib/vkauth /mnt/vkfs}
+fn k {kill Vkfs Nametree Styxservers}
+fn u {unmount /mnt/vkfs}
